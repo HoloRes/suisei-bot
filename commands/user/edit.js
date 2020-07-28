5f12d4df8e8787c9c00816098c7e96bfbdacb02c
GoldElysium <48455312+GoldElysium@users.noreply.github.com>
2020-07-28 23:33:59 +0200

    Add personal notes

--- commands/user/edit.js
+++ commands/user/edit.js
@@ -8,8 +8,17 @@ const fs = require("fs"),
     Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
-
+    /*
+    TODO:
+    1. Get ID and check against DB
+    2. Check if owner or staff, in case of staff and not owner: give confirmation embed
+    3. Ask what entry to edit (owner, author name or title) in embed
+    4. Ask the value, if owner: ping or id
+    5. Confirmation embed including the new value
+    6. Save/cancel changes
+    */
 }
+
 module.exports.config = {
     command: "edit"
 }
