637025c3784759bc3cd51e52231b65cf6975f7ec
GoldElysium <48455312+GoldElysium@users.noreply.github.com>
2020-08-08 16:34:17 +0200

    Fix code order

--- commands/staff/subscribe.js
+++ commands/staff/subscribe.js
@@ -31,122 +31,34 @@ exports.run = (client, message, args, pubSubSubscriber) => {
             });
         } else {
             client.channels.fetch(args[1])
-                .then((channel) => {
+                .then(async (channel) => {
                     let subscription = new Subscription({
                         _id: res.data.items[0].id,
                         channels: [channel.id],
                         message: args.slice(2).join(" ")
                     });
 
-                    let wh;
-                    channel.fetchWebhooks()
-                        .then((hooks) => {
-                            wh = hooks.find(wh => wh.name.toLowerCase() === "stream notification");
+                    await channel.fetchWebhooks()
+                        .then(async (hooks) => {
+                            const existingWebhook = await hooks.find(wh => wh.name.toLowerCase() === "stream notification");
+                            if (!existingWebhook) channel.createWebhook("Stream notification")
+                                .then((webhook) => {
+                                    checkExistingAndSubscribe(message, subscription, webhook, res, channel, pubSubSubscriber);
+                                })
+                                .catch((err) => {
+                                    if (err) {
+                                        return message.channel.send("Unable to create a webhook in that channel, please create one with the name `Stream notification`").then((msg) => {
+                                            message.delete({timeout: 4000, reason: "Automated"});
+                                            msg.delete({timeout: 4000, reason: "Automated"});
+                                        });
+                                    }
+                                });
+                            else checkExistingAndSubscribe(message, subscription, existingWebhook, res, channel, pubSubSubscriber);
                         });
 
-                    if (!wh) channel.createWebhook("Stream notification")
-                        .then((webhook) => {
-                            wh = webhook;
-                        })
-                        .catch((err) => {
-                            if (err) {
-                                return message.channel.send("Unable to create a webhook in that channel, please create one with the name `Stream notification`").then((msg) => {
-                                    message.delete({timeout: 4000, reason: "Automated"});
-                                    msg.delete({timeout: 4000, reason: "Automated"});
-                                });
-                            }
-                        });
 
-                    Subscription.findById(subscription._id, (err, sub) => {
-                        if (!err) {
-                            if (sub) {
-                                sub.channels.push(channel.id);
-                                return message.channel.send(`Are you sure you want to add ${res.data.items[0].snippet.title} to ${channel.name}?`)
-                                    .then(msg => {
-                                        confirmRequest(msg, message.author.id)
-                                            .then(result => {
-                                                msg.delete({reason: "Automated"})
-                                                if (result === true) {
-                                                    sub.save((err) => {
-                                                        if (err) message.channel.send("Something went wrong saving to the database.")
-                                                            .then(msg2 => {
-                                                                message.delete({timeout: 4000, reason: "Automated"});
-                                                                msg2.delete({timeout: 4000, reason: "Automated"});
-                                                            });
-                                                        else message.channel.send("Subscription successful.")
-                                                            .then(msg2 => {
-                                                                message.delete({timeout: 4000, reason: "Automated"});
-                                                                msg2.delete({timeout: 4000, reason: "Automated"});
-                                                            });
-                                                    });
-                                                }
-                                            });
-                                    });
-                            } else {
-                                const embed = new Discord.MessageEmbed()
-                                    //.setTitle("Playing: A Game")
-                                    .setTitle("Stream title")
-                                    .setImage("https://cdn.discordapp.com/attachments/730061446108676146/741281514091708537/Example_thumbnail.png")
-                                    .setColor("#FF0000")
-                                    .setFooter("Powered by Suisei's Mic")
 
-                                wh.send(subscription.message, {
-                                    embeds: [embed],
-                                    disableMentions: true,
-                                    username: res.data.items[0].snippet.title,
-                                    avatarURL: res.data.items[0].snippet.thumbnails.high.url
-                                }).then((exampleEmbed) => {
-                                    message.channel.send("Is this correct?").then((msg) => {
-                                        confirmRequest(msg, message.author.id)
-                                            .then(result => {
-                                                if (result === true) {
-                                                    exampleEmbed.delete({reason: "Automated"});
-                                                    msg.delete({timeout: 2000, reason: "Automated"});
-                                                    pubSubSubscriber.subscribe(
-                                                        `https://www.youtube.com/xml/feeds/videos.xml?channel_id=${subscription._id}`,
-                                                        "https://pubsubhubbub.appspot.com",
-                                                        (err) => {
-                                                            if (err) message.channel.send("Something went wrong during the subscription, try again later.")
-                                                                .then(msg2 => {
-                                                                    message.delete({
-                                                                        timeout: 4000,
-                                                                        reason: "Automated"
-                                                                    });
-                                                                    msg2.delete({timeout: 4000, reason: "Automated"});
-                                                                });
-                                                            else subscription.save((err2) => {
-                                                                if (err2) message.channel.send("Something went wrong during the subscription, try again later.")
-                                                                    .then(msg2 => {
-                                                                        message.delete({
-                                                                            timeout: 4000,
-                                                                            reason: "Automated"
-                                                                        });
-                                                                        msg2.delete({
-                                                                            timeout: 4000,
-                                                                            reason: "Automated"
-                                                                        });
-                                                                    });
-                                                                else message.channel.send("Subscription successful.")
-                                                                    .then(msg2 => {
-                                                                        message.delete({
-                                                                            timeout: 4000,
-                                                                            reason: "Automated"
-                                                                        });
-                                                                        msg2.delete({
-                                                                            timeout: 4000,
-                                                                            reason: "Automated"
-                                                                        });
-                                                                    });
-                                                            });
-                                                        }
-                                                    )
-                                                }
-                                            });
-                                    });
-                                });
-                            }
-                        }
-                    });
+
                 })
                 .catch((err) => {
                     if (err) {
@@ -156,6 +68,99 @@ exports.run = (client, message, args, pubSubSubscriber) => {
                         });
                     }
                 });
+        }
+    });
+}
+
+function checkExistingAndSubscribe(message, subscription, wh, res, channel, pubSubSubscriber) {
+    Subscription.findById(subscription._id, (err, sub) => {
+        if (!err) {
+            if (sub) {
+                sub.channels.push(channel.id);
+                return message.channel.send(`Are you sure you want to add ${res.data.items[0].snippet.title} to ${channel.name}?`)
+                    .then(msg => {
+                        confirmRequest(msg, message.author.id)
+                            .then(result => {
+                                msg.delete({reason: "Automated"})
+                                if (result === true) {
+                                    sub.save((err) => {
+                                        if (err) message.channel.send("Something went wrong saving to the database.")
+                                            .then(msg2 => {
+                                                message.delete({timeout: 4000, reason: "Automated"});
+                                                msg2.delete({timeout: 4000, reason: "Automated"});
+                                            });
+                                        else message.channel.send("Subscription successful.")
+                                            .then(msg2 => {
+                                                message.delete({timeout: 4000, reason: "Automated"});
+                                                msg2.delete({timeout: 4000, reason: "Automated"});
+                                            });
+                                    });
+                                }
+                            });
+                    });
+            } else {
+                const embed = new Discord.MessageEmbed()
+                    //.setTitle("Playing: A Game")
+                    .setTitle("Stream title")
+                    .setImage("https://cdn.discordapp.com/attachments/730061446108676146/741281514091708537/Example_thumbnail.png")
+                    .setColor("#FF0000")
+                    .setFooter("Powered by Suisei's Mic")
+
+                wh.send(subscription.message, {
+                    embeds: [embed],
+                    disableMentions: true,
+                    username: res.data.items[0].snippet.title,
+                    avatarURL: res.data.items[0].snippet.thumbnails.high.url
+                }).then((exampleEmbed) => {
+                    message.channel.send("Is this correct?").then((msg) => {
+                        confirmRequest(msg, message.author.id)
+                            .then(result => {
+                                if (result === true) {
+                                    exampleEmbed.delete({reason: "Automated"});
+                                    msg.delete({timeout: 2000, reason: "Automated"});
+                                    pubSubSubscriber.subscribe(
+                                        `https://www.youtube.com/xml/feeds/videos.xml?channel_id=${subscription._id}`,
+                                        "https://pubsubhubbub.appspot.com",
+                                        (err) => {
+                                            if (err) message.channel.send("Something went wrong during the subscription, try again later.")
+                                                .then(msg2 => {
+                                                    message.delete({
+                                                        timeout: 4000,
+                                                        reason: "Automated"
+                                                    });
+                                                    msg2.delete({timeout: 4000, reason: "Automated"});
+                                                });
+                                            else subscription.save((err2) => {
+                                                if (err2) message.channel.send("Something went wrong during the subscription, try again later.")
+                                                    .then(msg2 => {
+                                                        message.delete({
+                                                            timeout: 4000,
+                                                            reason: "Automated"
+                                                        });
+                                                        msg2.delete({
+                                                            timeout: 4000,
+                                                            reason: "Automated"
+                                                        });
+                                                    });
+                                                else message.channel.send("Subscription successful.")
+                                                    .then(msg2 => {
+                                                        message.delete({
+                                                            timeout: 4000,
+                                                            reason: "Automated"
+                                                        });
+                                                        msg2.delete({
+                                                            timeout: 4000,
+                                                            reason: "Automated"
+                                                        });
+                                                    });
+                                            });
+                                        }
+                                    )
+                                }
+                            });
+                    });
+                });
+            }
         }
     });
 }
