a327d02c6726cd6354717e655133bd967db4d60f
GoldElysium <48455312+GoldElysium@users.noreply.github.com>
2020-08-10 00:12:55 +0200

    Add custom code instead of module

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const fs = require("fs"),
     mongoose = require("mongoose"), // Library for MongoDB
     xml2js = require("xml2js"),
     express = require("express"),
-    PubHubSubBub = require("pubsubhubbub"), // Library for YouTube notifications
+    axios = require("axios"),
+    {createHmac} = require("crypto"),
+    {scheduleJob} = require("node-schedule"),
+    rawBody = require("raw-body"),
+    querystring = require("querystring"),
     {google} = require("googleapis");
 
 // Local JS files
@@ -26,48 +30,34 @@ mongoose.connect(`mongodb+srv://${config.mongodb.username}:${config.mongodb.pass
     useUnifiedTopology: true
 });
 
-// PubSubHubBub
-const pubHubSubscriber = PubHubSubBub.createServer({
-    secret: config.PubHubSubBub.secret,
-    callbackUrl: config.PubHubSubBub.callbackUrl
-});
-
 // Express
 const app = express();
 app.listen(config.PubHubSubBub.hubPort);
 
-app.use("/ytPush", pubHubSubscriber.listener());
-
-// Debug stuff
-pubHubSubscriber.on("subscribe", function(data){
-    console.log("Subscribe");
-    console.log(data);
-    console.log("-----------------------------------------");
-});
-
-pubHubSubscriber.on("unsubscribe", function(data){
-    console.log("Unsubscribe");
-    console.log(data);
-    console.log("-----------------------------------------");
-});
-
-pubHubSubscriber.on("error", function(error){
-    console.log("Error");
-    console.log(error);
-    console.log("-----------------------------------------");
-});
 
 // Add all subscriptions
-Subscription.find({}).lean().exec((err, docs) => {
-   if(err) throw new Error("Couldn't read subscriptions");
-   for(let i = 0; i < docs.length; i++) {
-       pubHubSubscriber.subscribe(
-           `https://www.youtube.com/xml/feeds/videos.xml?channel_id=${docs[i]._id}`,
-           "https://pubsubhubbub.appspot.com",
-           (err2) => {
-               if(err2) console.error(`Couldn't subscribe to channel: ${docs[i]._id}`);
-           });
-   }
+scheduleJob("0 * * * *", () => { // Resubscribe every hour
+    Subscription.find({}).lean().exec((err, docs) => {
+        if (err) throw new Error("Couldn't read subscriptions");
+        for (let i = 0; i < docs.length; i++) {
+            try {
+                axios({
+                    url: "http://pubsubhubbub.appspot.com/",
+                    method: "POST",
+                    headers: {"content-type": "application/x-www-form-urlencoded"},
+                    data: querystring.stringify({
+                        "hub.mode": "subscribe",
+                        "hub.callback": config.PubHubSubBub.callbackUrl,
+                        "hub.topic": `https://www.youtube.com/xml/feeds/videos.xml?channel_id=${docs[i]._id}`,
+                        "hub.lease_seconds": `${60 * 60}`, // 1 hour lease
+                        "hub.secret": config.PubHubSubBub.secret,
+                    }),
+                });
+            } catch (err2) {
+                if (err2) return console.error(`Couldn't subscribe to channel: ${docs[i]._id}`);
+            }
+        }
+    });
 });
 
 // Google YT Data V3 API
@@ -76,55 +66,63 @@ const YT = google.youtube("v3");
 // Code
 
 // PubSubHubBub notifications
-pubHubSubscriber.on("feed", (data) => {
-    console.log(data.feed.toString("utf-8"));
+app.get("/", (req, res) => {
+    if (req.query["hub.mode"] === "subscribe" && req.query["hub.challenge"].length > 0) res.status(200).send(req.query["hub.challenge"]);
+    else res.status(400).send("");
+});
+
+app.post("/", verifyHmac, (req, res) => {
+    console.log(req.body.feed.toString("utf-8"));
     console.log("-----------------------------------------");
+    res.status(200).send("");
     let removedChannels = [];
-    xmlParser.parseString(data.feed.toString("utf-8"), (err, res) => {
-        if (err) return;
+    xmlParser.parseString(req.body.feed.toString("utf-8"), (err, res) => {
+        if (err) return console.error(err);
         Subscription.findById(res.feed.entry["yt:channelId"], (err2, subscription) => {
             client.channels.fetch("730061446108676146")
                 .then((channel) => {
                     channel.send(JSON.stringify(res, null, 4), {code: "json"});
                 })
-                .catch(chErr => {return;});
-            if (err2) return;
+                .catch(chErr => {});
+            if (err2) return console.error(err2);
             YT.videos.list({
                 auth: config.YtApiKey,
                 id: res.feed.entry["yt:videoId"],
                 part: "snippet"
             }, (err3, video) => {
-                if(err3 || !video) return;
+                if (err3 || !video) return;
                 client.channels.fetch("730061446108676146")
                     .then((channel) => {
                         channel.send(JSON.stringify(video, null, 4), {code: "json"});
                     })
-                    .catch(chErr => {return;});
-                if(video.items[0].snippet.liveBroadcastContent !== "live") return;
+                    .catch(chErr => {});
+                if (video.items[0].snippet.liveBroadcastContent !== "live") return;
                 YT.channels.list({
                     auth: config.YtApiKey,
                     id: res.feed.entry["yt:channelId"],
                     part: "snippet"
                 }, (err4, ytChannel) => {
-                    if (err4) return;
+                    if (err4) return console.error(err4);
                     for (let i = 0; i < subscription.channels.length; i++) {
                         client.channels.fetch(subscription.channels[i])
                             .then((channel) => {
-                                const webhook = channel.fetchWebhooks().find(wh => wh.name.toLowerCase() === "stream notification");
-                                if (!webhook) return removedChannels.push(i);
-                                const embed = new Discord.MessageEmbed()
-                                    .setTitle(res.feed.entry.title)
-                                    .setURL(res.feed.entry.link["$"].href)
-                                    .setImage(video.items[0].snippet.thumbnails.maxres.url)
-                                    .setColor("#FF0000")
-                                    .setFooter("Powered by Suisei's Mic")
+                                channel.fetchWebhooks()
+                                    .then((hooks) => {
+                                        const webhook = hooks.find(wh => wh.name.toLowerCase() === "stream notification");
+                                        if (!webhook) return removedChannels.push(i);
+                                        const embed = new Discord.MessageEmbed()
+                                            .setTitle(res.feed.entry.title)
+                                            .setURL(res.feed.entry.link["$"].href)
+                                            .setImage(video.items[0].snippet.thumbnails.maxres.url)
+                                            .setColor("#FF0000")
+                                            .setFooter("Powered by Suisei's Mic")
 
-                                webhook.send(subscription.message, {
-                                    embeds: [embed],
-                                    username: ytChannel.items[0].snippet.title,
-                                    avatarURL: ytChannel.items[0].snippet.thumbnails.high
-                                });
-
+                                        webhook.send(subscription.message, {
+                                            embeds: [embed],
+                                            username: ytChannel.items[0].snippet.title,
+                                            avatarURL: ytChannel.items[0].snippet.thumbnails.high
+                                        });
+                                    });
                             })
                             .catch((err5) => {
                                 if (err5) removedChannels.push(i);
@@ -236,4 +234,21 @@ function loadcmds() {
             client.staffcmds.set(cmd.config.command, cmd);
         });
     });
+}
+
+async function verifyHmac(req, res, next) {
+    try {
+        const xhs = req.headers["x-hub-signature"] || req.headers["X-Hub-Signature"];
+        if (!xhs) return res.status(403).send("");
+        const method = xhs.split("=")[0];
+        const signature = xhs.split("=")[1];
+        const raw = await rawBody(req);
+
+        const hmac = createHmac(method, config.PubHubSubBub.secret);
+        hmac.update(raw);
+        if (signature !== `${method}=${hmac.digest("hex")}`) return res.status(403).send("");
+        next();
+    } catch (error) {
+        next(error);
+    }
 }
