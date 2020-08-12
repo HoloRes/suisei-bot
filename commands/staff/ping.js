9edeb6c5f089233ad17f0137f977d62d6cb619e1
GoldElysium <48455312+GoldElysium@users.noreply.github.com>
2020-08-13 00:15:56 +0200

    Hotfix

--- commands/staff/ping.js
+++ commands/staff/ping.js
@@ -8,13 +8,12 @@ const {confirmRequest} = require("$/util/functions"),
 exports.run = (client, message, args) => {
     client.channels.fetch(args[0])
         .then((channel) => {
-            PingSubscription.find({name: args.slice(1).join(" ")}).lean().exec((err, doc) => {
+            PingSubscription.findOne({name: args.slice(1).join(" ")}).lean().exec((err, doc) => {
                 if (!doc) return message.channel.send("That list doesn't exist.")
                     .then((msg) => {
                         message.delete({timeout: 4000, reason: "Automated"});
                         msg.delete({timeout: 4000, reason: "Automated"});
                     });
-                logger.debug(doc);
                 message.channel.send(`Are you sure you want to ping everyone in: ${doc.name}?`)
                     .then((msg) => {
                         confirmRequest(msg, message.author.id)
