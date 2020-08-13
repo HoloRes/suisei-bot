e83a2e975a7148b9df5dcf06cfedddc7143b56bb
GoldElysium <48455312+GoldElysium@users.noreply.github.com>
2020-08-13 17:43:47 +0200

    Add unicode emoji support

--- commands/staff/createpinglist.js
+++ commands/staff/createpinglist.js
@@ -2,7 +2,8 @@
 const PingSubscription = require("$/models/pingSubscription");
 
 // Local files
-const {confirmRequest} = require("$/util/functions");
+const {confirmRequest} = require("$/util/functions"),
+    unicodeEmojis = require("$/util/unicodeEmojis.json");
 
 // Modules
 const Discord = require("discord.js")
@@ -11,41 +12,20 @@ exports.run = (client, message, args) => {
     client.channels.fetch(args[0])
         .then((channel) => {
             const name = args.slice(2).join(" ");
-            const emoji = message.guild.emojis.resolve(args[1]);
-            if (!emoji) return message.reply("That emote doesn't exist.")
-                .then((msg) => {
-                    message.delete({timeout: 4000, reason: "Automated"});
-                    msg.delete({timeout: 4000, reason: "Automated"});
-                });
-            message.reply(`are you sure you want to create a new ping list with the name ${name}?`)
-                .then((msg) => {
-                    confirmRequest(msg, message.author.id)
-                        .then((result) => {
-                            if (result === true) {
-                                const embed = new Discord.MessageEmbed()
-                                    .setTitle(name)
-                                    .setDescription(`React with <:${emoji.name}:${emoji.id}> to subscribe to this ping list`)
-                                channel.send(embed).then((embedMsg) => {
-                                    embedMsg.react(emoji);
-                                    const ping = new PingSubscription({
-                                        _id: embedMsg.id,
-                                        channelID: embedMsg.channel.id,
-                                        users: [],
-                                        name: name,
-                                        emoji: emoji.id
-                                    });
-                                    ping.save();
-                                    msg.edit("New list created");
-                                    message.delete({timeout: 4000, reason: "Automated"});
-                                    msg.delete({timeout: 4000, reason: "Automated"});
-                                });
-                            } else {
-                                msg.edit("Action cancelled");
-                                message.delete({timeout: 4000, reason: "Automated"});
-                                msg.delete({timeout: 4000, reason: "Automated"});
-                            }
-                        })
-                });
+            if(unicodeEmojis[args[0]]) {
+                const emoji = unicodeEmojis[args[0]];
+                createList(message, channel, emoji, name);
+            }
+            else {
+                const emoji = message.guild.emojis.resolve(args[1]);
+                if (!emoji) return message.reply("That emote doesn't exist.")
+                    .then((msg) => {
+                        message.delete({timeout: 4000, reason: "Automated"});
+                        msg.delete({timeout: 4000, reason: "Automated"});
+                    });
+                else createList(message, channel, emoji, name)
+            }
+
         })
         .catch((err) => {
             if (err) return message.reply("That channel doesn't exist.")
@@ -58,4 +38,36 @@ exports.run = (client, message, args) => {
 
 module.exports.config = {
     command: "createpinglist"
+}
+
+function createList(message, channel, emoji, name) {
+    message.reply(`are you sure you want to create a new ping list with the name ${name}?`)
+        .then((msg) => {
+            confirmRequest(msg, message.author.id)
+                .then((result) => {
+                    if (result === true) {
+                        const embed = new Discord.MessageEmbed()
+                            .setTitle(name)
+                            .setDescription(`React with <:${emoji.name}:${emoji.id}> to subscribe to this ping list`)
+                        channel.send(embed).then((embedMsg) => {
+                            embedMsg.react(emoji);
+                            const ping = new PingSubscription({
+                                _id: embedMsg.id,
+                                channelID: embedMsg.channel.id,
+                                users: [],
+                                name: name,
+                                emoji: emoji.id
+                            });
+                            ping.save();
+                            msg.edit("New list created");
+                            message.delete({timeout: 4000, reason: "Automated"});
+                            msg.delete({timeout: 4000, reason: "Automated"});
+                        });
+                    } else {
+                        msg.edit("Action cancelled");
+                        message.delete({timeout: 4000, reason: "Automated"});
+                        msg.delete({timeout: 4000, reason: "Automated"});
+                    }
+                })
+        });
 }
