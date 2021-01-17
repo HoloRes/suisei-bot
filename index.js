9b0092ae0d06e2fee4e1cddff3d843b13c0b35e2
GoldElysium <48455312+GoldElysium@users.noreply.github.com>
2021-01-17 16:36:17 +0100

    Remove unused commands

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ const client = new Discord.Client({
 exports.client = client;
 
 // Local JS files
-const {confirmRequest} = require("$/util/functions"),
-    youtubeNotifications = require("$/util/youtube"),
+const youtubeNotifications = require("$/util/youtube"),
     twitterNotifications = require("$/util/twitter"),
     dashboardRouter = require("$/routers/dashboard");
 
@@ -77,7 +76,7 @@ client.on("ready", () => {
 });
 
 // Ping list reaction handler
-client.on('messageReactionAdd', (reaction, user) => {
+client.on("messageReactionAdd", (reaction, user) => {
     if (user.id === client.user.id) return;
     reaction.fetch().then((reaction) => {
         PingSubscription.findById(reaction.message.id, (err, doc) => {
@@ -93,7 +92,7 @@ client.on('messageReactionAdd', (reaction, user) => {
     });
 });
 
-client.on('messageReactionRemove', (reaction, user) => {
+client.on("messageReactionRemove", (reaction, user) => {
     if (user.id === client.user.id) return;
     reaction.fetch().then((reaction) => {
         PingSubscription.findById(reaction.message.id, (err, doc) => {
@@ -123,6 +122,7 @@ client.on("message", (message) => {
                         'Authorization': `Bot ${config.discord.token}`,
                     }
                 })
+                    .catch(() => {}); // Ignore errors
             } else {
                 doc.autoPublish = false;
                 doc.save();
@@ -137,31 +137,12 @@ client.on("message", (message) => {
     if (message.content.startsWith(config.discord.prefix)) { // User command handler
         let cont = message.content.slice(config.discord.prefix.length).split(" ");
         let args = cont.slice(1);
+
         let staffCmd = client.staffcmds.get(cont[0]);
         if(staffCmd && message.member.roles.cache.has(config.discord.roles.staff)) return staffCmd.run(client, message, args);
         let cmd = client.commands.get(cont[0]);
-        if (!cmd) return;
-        if(cmd.config.limited === "musician") {
-            if (!message.member.roles.cache.has(config.discord.roles.musician) && !message.member.roles.cache.has(config.discord.roles.staff)) return;
-            if (!message.member.roles.cache.has(config.discord.roles.musician)) {
-                message.reply("you don't have the musician role, do you still want to perform this action?").then(msg => {
-                    confirmRequest(msg, message.author.id)
-                        .then(result => {
-                            if (result === true) {
-                                msg.delete({reason: "Automated"});
-                                return cmd.run(client, message, args);
-                            } else {
-                                message.delete({reason: "Automated"});
-                                return msg.delete({reason: "Automated"});
-                            }
-                        });
-                });
 
-            } else {
-                return cmd.run(client, message, args);
-            }
-        }
-        else return cmd.run(client, message, args);
+        if (cmd) return cmd.run(client, message, args);
     } else if (message.content.startsWith(config.discord.devprefix)) { // Dev command handler
         if (!message.member.roles.cache.has(config.discord.roles.dev)) return;
         let cont = message.content.slice(config.discord.devprefix.length).split(" ");
@@ -180,13 +161,28 @@ client.login(config.discord.token);
 
 // Functions
 function loadcmds() {
+    // Unload all loaded commands in case it's a reload
+    client.commands.forEach((cmd) => {
+        client.commands.delete(cmd.config.command);
+        delete require.cache[require.resolve(`./commands/user/${cmd.config.command}.js`)];
+    });
+    client.devcmds.forEach((cmd) => {
+        client.devcmds.delete(cmd.config.command);
+        delete require.cache[require.resolve(`./commands/user/${cmd.config.command}.js`)];
+    });
+    client.staffcmds.forEach((cmd) => {
+        client.staffcmds.delete(cmd.config.command);
+        delete require.cache[require.resolve(`./commands/user/${cmd.config.command}.js`)];
+    });
+
+    // Fetch all files, filter and load them
     fs.readdir("./commands/user", (err, files) => { // Read all the files in the directory, these are commands available to Musicians. Override available for staff.
         if (err) throw (err);
         let jsfiles = files.filter(f => f.split(".").pop() === "js");
         if (jsfiles.length <= 0) {
             return logger.info("No user commands found.");
         }
-        jsfiles.forEach((f, i) => {
+        jsfiles.forEach((f) => {
             delete require.cache[require.resolve(`./commands/user/${f}`)];
             const cmd = require(`./commands/user/${f}`);
             client.commands.set(cmd.config.command, cmd);
@@ -198,7 +194,7 @@ function loadcmds() {
         if (jsfiles.length <= 0) {
             return logger.info("No dev commands found.");
         }
-        jsfiles.forEach((f, i) => {
+        jsfiles.forEach((f) => {
             delete require.cache[require.resolve(`./commands/dev/${f}`)];
             const cmd = require(`./commands/dev/${f}`);
             client.devcmds.set(cmd.config.command, cmd);
@@ -210,7 +206,7 @@ function loadcmds() {
         if (jsfiles.length <= 0) {
             return logger.info("No staff commands found.");
         }
-        jsfiles.forEach((f, i) => {
+        jsfiles.forEach((f) => {
             delete require.cache[require.resolve(`./commands/staff/${f}`)];
             const cmd = require(`./commands/staff/${f}`);
             client.staffcmds.set(cmd.config.command, cmd);
