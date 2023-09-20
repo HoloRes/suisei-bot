d3b380397b6fa440b342e0acb963437680b31f65
GoldElysium <goldelysium@gmail.com>
2023-09-20 11:43:26 +0200

    feat: add crossreport command

--- src/modules/moderation/commands/report.ts
+++ src/modules/moderation/commands/report.ts
@@ -1,6 +1,13 @@
 import { Command } from '@sapphire/framework';
 import { ApplyOptions } from '@sapphire/decorators';
-import { PermissionFlagsBits } from 'discord.js';
+import {
+	ActionRowBuilder,
+	ComponentType,
+	PermissionFlagsBits,
+	StringSelectMenuBuilder,
+	StringSelectMenuOptionBuilder,
+} from 'discord.js';
+import { UserReportType } from '@holores/bansapi.js/dist/types';
 
 @ApplyOptions<Command.Options>({
 	name: 'report',
@@ -24,6 +31,76 @@ export class ReportCommand extends Command {
 	}
 
 	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		await interaction.reply('Not implemented yet.');
+		if (!interaction.inGuild()) {
+			await interaction.reply('This command cannot run outside a guild');
+			return;
+		}
+
+		const user = interaction.options.getUser('user', true);
+		const reason = interaction.options.getString('reason', true);
+
+		const typeSelect = new StringSelectMenuBuilder()
+			.setCustomId('type')
+			.setPlaceholder('Choose a report type')
+			.addOptions(
+				new StringSelectMenuOptionBuilder()
+					.setValue('NORMAL')
+					.setLabel('Normal')
+					.setDescription('A normal ban'),
+				new StringSelectMenuOptionBuilder()
+					.setValue('COMPROMISED')
+					.setLabel('Compromised')
+					.setDescription('A compromised account'),
+			);
+
+		const row = new ActionRowBuilder<StringSelectMenuBuilder>()
+			.addComponents(typeSelect);
+
+		const followUp = await interaction.reply({
+			components: [row],
+		});
+
+		let type;
+		try {
+			type = await followUp.awaitMessageComponent({
+				componentType: ComponentType.StringSelect,
+				time: 900_000,
+			});
+		} catch {
+			await followUp.edit({ content: 'Confirmation not received within 15 minutes, cancelling', components: [] });
+			return;
+		}
+
+		const report = await this.container.bansApi.users.create({
+			userId: user.id,
+			moderatorId: interaction.user.id,
+			reason,
+			type: type.values[0] as UserReportType,
+		});
+
+		await this.container.retracedClient.reportEvent({
+			action: 'moderation.crossreport.user',
+			group: {
+				id: interaction.guildId,
+				name: interaction.guild!.name,
+			},
+			crud: 'c',
+			actor: {
+				id: interaction.user.id,
+				name: interaction.user.tag,
+			},
+			target: {
+				id: user.id,
+				type: 'User',
+			},
+			fields: {
+				reportId: report.id.toString(),
+			},
+		});
+
+		await interaction.editReply({
+			content: `Successfully reported, id: ${report.id}`,
+			components: [],
+		});
 	}
 }
